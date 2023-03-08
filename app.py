0e9fd6ec2addb516fb45e2e3ceae0dc1539678d4
idanlevi74 <119952492+idanlevi74@users.noreply.github.com>
2023-03-08 14:22:09 +0000

    change

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from views import views
+from auth import auth
 
 app = Flask(__name__)
+
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+##############################################################
+"""
+#from .views import views
+#from .auth import auth
+app = Flask(__name__)
+
+
+#app.register_blueprint(views, url_prefix='/')
+#app.register_blueprint(auth, url_prefix='/')
 
 
 @app.route("/")
@@ -10,3 +27,5 @@ def hll():
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
+"""
